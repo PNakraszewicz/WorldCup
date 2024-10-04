89db3e3ef944df194bd4b458dcf0472502c4c3f7
pawel.nakraszewicz <pawel.nakraszewicz@capgemini.com>
2024-10-04 12:26:00 +0200

    Implement startGame method

--- src/main/java/com/interview/worldcup/ScoreBoard.java
+++ src/main/java/com/interview/worldcup/ScoreBoard.java
@@ -2,12 +2,15 @@ package com.interview.worldcup;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 public class ScoreBoard {
 
     List<Game> runningGames = new ArrayList<>();
 
-    public List<Game> startGame(final Team homeTeam, final Team awayTeam) {
+    public List<Game> startGame(Team homeTeam, Team awayTeam) {
+        Game game = new Game(homeTeam, awayTeam);
+        runningGames.add(game);
         return runningGames;
     }
 }
